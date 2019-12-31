e7356c5f196ac8bd0e39adfc3a87d3f6d2b0b94f
joecai6 <55220898+joecai6@users.noreply.github.com>
2019-12-30 17:32:55 -0800

    calculator functionality

--- GradeCalc/README.txt
+++ GradeCalc/README.txt
@@ -5,3 +5,12 @@ Started 12/28/19
 - Created using Android Studio / Java
 - Created Android Emulator
 
+12/29/19
+
+- Started incorporating buttons and text
+
+12/30/19
+
+- Started calculator
+
+
--- GradeCalc/app/src/main/java/com/example/gradecalculator/Course.kt
+++ GradeCalc/app/src/main/java/com/example/gradecalculator/Course.kt
@@ -3,11 +3,21 @@ package com.example.gradecalculator
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_course.*
+import kotlin.text.*;
 
 //new screen (activity)
 class Course : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course)
+
+        calcButton.setOnClickListener {
+            var sum = firstGradeText.text.toString().toInt() +
+                    secondGradeText.text.toString().toInt()
+            resultTextView.text = sum.toString()
+        }
     }
+
+
 }
