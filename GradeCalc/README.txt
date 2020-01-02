f8d47dff4ace808b626bda490dcc13dfd6847b9d
joecai6 <55220898+joecai6@users.noreply.github.com>
2020-01-01 23:50:53 -0800

    bug fixes

--- GradeCalc/README.txt
+++ GradeCalc/README.txt
@@ -13,9 +13,13 @@ Started 12/28/19
 
 - Started calculator
 
+1/1/19
+- Bug fixes
+
 
 Incorporate
 	- Final Grade calculator
+		- Clear / Add entry button
 		- Grade needed for expected overall 
 		- Adding / Deleting entries
 		- Convenient Use with keyboard
--- GradeCalc/app/src/main/java/com/example/gradecalculator/Course.kt
+++ GradeCalc/app/src/main/java/com/example/gradecalculator/Course.kt
@@ -2,6 +2,7 @@ package com.example.gradecalculator
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_course.*
 import kotlin.text.*;
@@ -13,19 +14,41 @@ class Course : AppCompatActivity() {
         setContentView(R.layout.activity_course)
 
         calcButton.setOnClickListener {
-            var p1 = firstGradeText.text.toString().toInt() *
-                    firstWeightText.text.toString().toInt()
-            var p2 = secondGradeText.text.toString().toInt() *
-                    secondWeightText.text.toString().toInt()
-            var p3 = thirdGradeText.text.toString().toInt() *
-                    thirdWeightText.text.toString().toInt()
-            var p4 = forthGradeText.text.toString().toInt() *
-                    forthWeightText.text.toString().toInt()
-            var p5 = fifthGradeText.text.toString().toInt() *
-                    fifthWeightText.text.toString().toInt()
-            resultTextView.text = (p1 + p2 + p3 + p4 + p5).toString()
+
+            var p1 = if(firstWeightText.text.toString()=="" || firstGradeText.text.toString()=="")
+                    0.0
+                    else firstGradeText.text.toString().toDouble() *
+                    firstWeightText.text.toString().toDouble()/100
+
+            var p2 = if(secondWeightText.text.toString()=="" || secondGradeText.text.toString()=="")
+                    0.0
+                    else secondGradeText.text.toString().toDouble() *
+                    secondWeightText.text.toString().toDouble()/100
+
+            var p3= if(thirdWeightText.text.toString()=="" || thirdGradeText.text.toString()=="")
+                    0.0
+                    else thirdGradeText.text.toString().toDouble() *
+                    thirdWeightText.text.toString().toDouble()/100
+
+            var p4 = if(forthWeightText.text.toString()=="" || forthGradeText.text.toString()=="")
+                    0.0
+                    else forthGradeText.text.toString().toDouble() *
+                    forthWeightText.text.toString().toDouble()/100
+
+            var p5 = if(fifthWeightText.text.toString()=="" || fifthGradeText.text.toString()=="")
+                    0.0
+                    else fifthGradeText.text.toString().toDouble() *
+                    fifthWeightText.text.toString().toDouble()/100
+
+            resultTextView.text =  String.format("%.3f", p1 + p2 + p3 + p4 + p5)
+
+            Log.d("print", resultTextView.text.toString())
+        }
+
+        addEntryButton.setOnClickListener {
 
         }
+
     }
 
 
